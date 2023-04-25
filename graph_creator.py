6d63c302dac7b1a125427b53c545322f3db5eb47
Kilapang Huichalaf <kilapang.huichalaf@etu.utc.fr>
2023-04-26 00:25:38 +0200

    Functions to create graph

--- graph_creator.py
+++ graph_creator.py
@@ -1,14 +1,76 @@
+import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import random
 
 
-def create_random_graph(num_vertices, num_edges):
+def create_random_vertices(num_vertices):
+    """
+    Create two vertices within the existing graph's nodes
+    The vertices are not equal
+    :param num_vertices: int
+    :return: vertex1, vertex2
+    """
+    equals = True
+    while equals:
+        vertex1 = random.randint(0, num_vertices - 1)
+        vertex2 = random.randint(0, num_vertices - 1)
+        if vertex1 != vertex2:
+            equals = False
+    return vertex1, vertex2
+
+def create_base_graph(num_vertices, num_edges):
+    max_edges = ((num_vertices-1)*num_vertices)/2 # Max edges of a Directed Acyclic Graph (DAG)
+    if num_edges > max_edges:
+        raise Exception("Trying to create a graph with too many edges")
     graph = nx.DiGraph()
     vertices = np.arange(num_vertices)
     graph.add_nodes_from(vertices)
+    return graph
+
+
+def create_random_DiGraph(num_vertices, num_edges):
+    graph = create_base_graph(num_vertices, num_edges)
     for i in range(num_edges):
-        vertex1 = random.randint(0, num_vertices - 1)
-        vertex2 = random.randint(0, num_vertices - 1)
+        vertex1, vertex2 = create_random_vertices(num_vertices)
         graph.add_edge(vertex1, vertex2)
     return graph
+
+
+def create_acyclic_DiGraph(num_vertices, num_edges):
+    graph = create_base_graph(num_vertices, num_edges)
+    for i in range(num_edges):
+        not_acyclic = True
+        while not_acyclic:
+            vertex1, vertex2 = create_random_vertices(num_vertices)
+            if vertex2 not in nx.ancestors(graph, vertex1):
+                not_acyclic = False
+        graph.add_edge(vertex1, vertex2)
+    return graph
+
+def draw_graph(graph : nx.Graph):
+    options = {
+        'node_color': 'red',
+        'node_size': 200,
+        'with_labels' : True,
+        'width': 3,
+    }
+    return nx.draw_networkx(graph, pos=nx.spring_layout(graph), **options)
+
+def draw_grey_graph(graph : nx.Graph):
+    options = {
+        'node_color': 'grey',
+        'node_size': 200,
+        'with_labels': True,
+        'alpha': 0.5,
+        'width': 3,
+        'edge_color': 'grey',
+        'arrowsize': 20
+    }
+    return nx.draw_networkx(graph, **options)
+
+
+graph = create_acyclic_DiGraph(4, 6)
+draw_graph(graph)
+plt.show()
+#%%
